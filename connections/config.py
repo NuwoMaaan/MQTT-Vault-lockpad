ce0beb6cb72cc49420b74abce379e554fa7c4ce6
NuwoMaaan <Leoholden24@outlook.com>
2026-02-24 22:45:30 +1100

    move all sub directories out of 'Main' directory

--- connections/config.py
+++ connections/config.py
@@ -1,6 +1,4 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from typing import Any
-
 
 class Settings(BaseSettings):
 
--- emqx/init.py
+++ emqx/init.py
@@ -1,6 +1,5 @@
 import requests
 import json
-import time
 
 from connections.config import settings
 from pathlib import Path
