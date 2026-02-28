690ee4851aabab5990ea7116b32e715691d4b4ef
NuwoMaaan <Leoholden24@outlook.com>
2026-02-28 18:12:21 +1100

    device_id rename to id for mongo payload template

--- backend/vaultpadlock/repository.py
+++ backend/vaultpadlock/repository.py
@@ -1,13 +1,13 @@
 from pymongo.collection import Collection
 from pymongo.database import Database
 from vaultpadlock.schemas import VaultPadlockEvents, VaultPadlockMetrics, VaultPadlockStatus
-import datetime
+from datetime import datetime
 from typing import TypeVar, Type
 
 
 T = TypeVar("T", VaultPadlockStatus, VaultPadlockEvents, VaultPadlockMetrics)
 
-def fetch_logs(schema: Type[T], collection: Collection, start: datetime,end: datetime) -> list[T]:
+def fetch_logs(schema: Type[T], collection: Collection, start: datetime, end: datetime) -> list[T]:
     
     cursor = collection.find({"timestamp": {"$gte": start, "$lte": end}}, {"_id": False})
     return [schema(**doc) for doc in cursor]
