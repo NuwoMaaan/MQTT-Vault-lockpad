1cdd543cd416a74c4b67c27d77e74f05a6d77448
NuwoMaaan <Leoholden24@outlook.com>
2026-03-06 21:28:03 +1100

    jwt auth set up

---
+++ backend/auth/security/jwt_handler.py
@@ -0,0 +1,28 @@
+from auth.config import settings
+from fastapi import HTTPException, status
+from datetime import datetime, UTC, timedelta
+import jwt
+
+SECRET_KEY = settings.JWT_SECRET_KEY
+EXPIRE = 60
+ALGORITHM = settings.ALGORITHM
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(UTC) + timedelta(minutes=EXPIRE)
+    to_encode.update({"exp": expire})
+
+    token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return token
+
+def verify_token(token: str):
+    try: 
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expire")
+
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
